f64e550f3a27e074e4e6568d4c3d80b3681e6c70
JeongSeok <ljs0429777@gmail.com>
2024-11-24 18:07:17 +0900

    Initial commit

---
+++ src/main/java/com/jeongseok/pinkybrainbo/PinkyBrainBoApplication.java
@@ -0,0 +1,13 @@
+package com.jeongseok.pinkybrainbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkyBrainBoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinkyBrainBoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeongseok/pinkybrainbo/PinkyBrainBoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeongseok.pinkybrainbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinkyBrainBoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
