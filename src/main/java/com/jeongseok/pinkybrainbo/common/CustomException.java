c48d3d8234fd0cbb582d55408e05579a9e8fad69
JeongSeok <ljs0429777@gmail.com>
2024-12-04 03:39:07 +0900

    feat: 공통 응답 객체 생성

---
+++ src/main/java/com/jeongseok/pinkybrainbo/common/CustomException.java
@@ -0,0 +1,15 @@
+package com.jeongseok.pinkybrainbo.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException{
+
+	private final ErrorCode errorCode;
+
+	public String getMessage() {
+		return errorCode.getMessage();
+	}
+}
