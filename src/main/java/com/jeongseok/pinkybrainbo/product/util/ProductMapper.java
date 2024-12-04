13ac67e1fac1a3c72df749c20d3b7eaad3a8899d
JeongSeok <ljs0429777@gmail.com>
2024-12-04 16:39:58 +0900

    update: 누락된 category 컬럼 추가

--- src/main/java/com/jeongseok/pinkybrainbo/product/util/ProductMapper.java
+++ src/main/java/com/jeongseok/pinkybrainbo/product/util/ProductMapper.java
@@ -1,15 +1,24 @@
 package com.jeongseok.pinkybrainbo.product.util;
 
 import com.jeongseok.pinkybrainbo.product.domain.Product;
-import com.jeongseok.pinkybrainbo.product.dto.CreateProductDto;
+import com.jeongseok.pinkybrainbo.product.dto.ProductDto;
 
 public class ProductMapper {
+	
 
-	public static Product toProduct(CreateProductDto createProductDto) {
+	public static Product toProduct(ProductDto.Request createProductRequest) {
 		return Product.builder()
-			.name(createProductDto.getName())
-			.description(createProductDto.getDescription())
+			.name(createProductRequest.getName())
+			.category(createProductRequest.getCategory())
+			.description(createProductRequest.getDescription())
 			.build();
 	}
 
+	public static ProductDto.Response toDto(Product savedProduct) {
+		return ProductDto.Response.builder()
+			.name(savedProduct.getName())
+			.category(savedProduct.getCategory())
+			.description(savedProduct.getDescription())
+			.build();
+	}
 }
