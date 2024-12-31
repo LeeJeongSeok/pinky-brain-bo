7890888fed33b66325816e81c7a7711891a79d72
JeongSeok <ljs0429777@gmail.com>
2024-12-31 18:34:17 +0900

    update: 상품 이미지 생성 요청 dto 변경 및 메소드명 변경

--- src/main/java/com/jeongseok/pinkybrainbo/product_image/util/ProductImageMapper.java
+++ src/main/java/com/jeongseok/pinkybrainbo/product_image/util/ProductImageMapper.java
@@ -2,35 +2,48 @@ package com.jeongseok.pinkybrainbo.product_image.util;
 
 import com.jeongseok.pinkybrainbo.product_image.domain.ProductImage;
 import com.jeongseok.pinkybrainbo.product_image.dto.ProductImageDto;
+import com.jeongseok.pinkybrainbo.product_image.dto.request.AddProductImageRequest;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductImageMapper {
 
-	public static List<ProductImage> toProductImages(List<ProductImageDto.Request> productImageDtos) {
+	// 생성
+	public static List<ProductImage> toDomain(List<AddProductImageRequest> productImageDtos) {
 		List<ProductImage> productImages = new ArrayList<>();
 		int order = 1; // 상품별로 순서를 초기화
 
-		for (ProductImageDto.Request productImageDto : productImageDtos) {
-			productImages.add(toProductImage(productImageDto, order++));
+		for (AddProductImageRequest productImageDto : productImageDtos) {
+			productImages.add(toDomain(productImageDto, order++));
 		}
 
 		return productImages;
 	}
 
-	public static List<ProductImage> toUpdateProductImages(List<ProductImageDto.Request> productImageDtos, int order) {
+	// 수정
+	public static List<ProductImage> toDomain(List<AddProductImageRequest> productImageDtos, int order) {
 		List<ProductImage> productImages = new ArrayList<>();
 
-		for (ProductImageDto.Request productImageDto : productImageDtos) {
-			productImages.add(toProductImage(productImageDto, ++order));
+		for (AddProductImageRequest productImageDto : productImageDtos) {
+			productImages.add(toDomain(productImageDto, ++order));
 		}
 
 		return productImages;
 	}
 
-	private static ProductImage toProductImage(ProductImageDto.Request productImageDto, int order) {
+	public static List<ProductImageDto.Response> toDto(List<ProductImage> productImages) {
+		List<ProductImageDto.Response> productImageDtos = new ArrayList<>();
+
+		for (ProductImage productImage : productImages) {
+			productImageDtos.add(toDto(productImage));
+		}
+
+		return productImageDtos;
+	}
+
+	private static ProductImage toDomain(AddProductImageRequest addProductImageRequest, int order) {
 		return ProductImage.builder()
-			.imageUrl(productImageDto.getStoreFileName())
+			.imageUrl(addProductImageRequest.getStoreFileName())
 			.imageOrder(order)
 			.build();
 	}
@@ -43,13 +56,5 @@ public class ProductImageMapper {
 			.build();
 	}
 
-	public static List<ProductImageDto.Response> toDto(List<ProductImage> productImages) {
-		List<ProductImageDto.Response> productImageDtos = new ArrayList<>();
 
-		for (ProductImage productImage : productImages) {
-			productImageDtos.add(toDto(productImage));
-		}
-
-		return productImageDtos;
-	}
 }
