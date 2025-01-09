21772b910b0bd27804f9eeb8fe0faf406654f8e9
JeongSeok <ljs0429777@gmail.com>
2025-01-09 19:00:15 +0900

    update: ResponseStatus 어노테이션을 사용하여 컨트롤러에서 직관적으로 알 수 있게 업데이트

--- src/main/java/com/jeongseok/pinkybrainbo/controller/ProductRestController.java
+++ src/main/java/com/jeongseok/pinkybrainbo/controller/ProductRestController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -32,12 +34,14 @@ public class ProductRestController {
 	private final ProductService productService;
 
 	@PostMapping(value = "/products")
+	@ResponseStatus(value = HttpStatus.CREATED)
 	public ApiResponse<ProductResponse> createProduct(@Valid @ModelAttribute AddProductRequest addProductRequest) throws IOException {
 
 		return ApiResponse.created(productService.createProduct(addProductRequest));
 	}
 
 	@GetMapping(value = "/products")
+	@ResponseStatus(value = HttpStatus.OK)
 	public ApiResponse<List<ProductResponse>> getProducts(@RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size, @RequestParam("keyword") Optional<String> keyword) {
 
 		// 페이징 처리
@@ -52,6 +56,7 @@ public class ProductRestController {
 	}
 
 	@GetMapping("/products/{id}")
+	@ResponseStatus(value = HttpStatus.OK)
 	public ApiResponse<ProductResponse> getProduct(@PathVariable("id") long id) {
 
 		ProductResponse productResponse = productService.getProduct(id);
@@ -60,6 +65,7 @@ public class ProductRestController {
 	}
 
 	@PatchMapping("/products/{id}")
+	@ResponseStatus(value = HttpStatus.OK)
 	public ApiResponse<ProductResponse> updateProduct(@PathVariable("id") long id, @ModelAttribute ModifyProductRequest modifyProductRequest) throws IOException {
 
 		ProductResponse updateProductDto = productService.updateProduct(id, modifyProductRequest);
@@ -68,6 +74,7 @@ public class ProductRestController {
 	}
 
 	@DeleteMapping("/products/{id}")
+	@ResponseStatus(value = HttpStatus.OK)
 	public ApiResponse<Void> deleteProduct(@PathVariable("id") long id) {
 
 		productService.deleteProduct(id);
