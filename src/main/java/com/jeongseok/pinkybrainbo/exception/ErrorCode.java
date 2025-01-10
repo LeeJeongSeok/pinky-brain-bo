8eda0ea28e196b0f6440390547eeaecb9affbce8
JeongSeok <ljs0429777@gmail.com>
2025-01-10 17:32:33 +0900

    update: 상황별 맞는 에러 코드 주석 정의

--- src/main/java/com/jeongseok/pinkybrainbo/exception/ErrorCode.java
+++ src/main/java/com/jeongseok/pinkybrainbo/exception/ErrorCode.java
@@ -9,7 +9,16 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ErrorCode {
 
+	/**
+	 * 404 Not Found
+	 */
+	NOT_FOUND_PRODUCT(HttpStatus.NOT_FOUND, "존재하지 않는 상품 정보입니다.", false),
+	NOT_FOUND_PRODUCT_IMAGE(HttpStatus.NOT_FOUND, "존재하지 않는 상품 이미지 정보입니다.", false),
 	NOT_FOUND_END_POINT(HttpStatus.NOT_FOUND, "존재하지 않는 API입니다.", false),
+
+	/**
+	 * 500 Internal Server Error
+	 */
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.", false),
 	;
 
--- src/main/java/com/jeongseok/pinkybrainbo/exception/SuccessCode.java
+++ src/main/java/com/jeongseok/pinkybrainbo/exception/SuccessCode.java
@@ -9,11 +9,18 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SuccessCode {
 
-	PRODUCT_CREATE_SUCCESS(HttpStatus.OK, "상품 생성 성공", true),
+	/**
+	 * 200 OK
+	 */
 	PRODUCT_LIST_GET_SUCCESS(HttpStatus.OK, "상품 리스트 조회 성공", true),
 	PRODUCT_GET_SUCCESS(HttpStatus.OK, "상품 조회 성공", true),
 	PRODUCT_UPDATE_SUCCESS(HttpStatus.OK, "상품 수정 성공", true),
-	PRODUCT_DELETE_SUCCESS(HttpStatus.OK, "상품 삭제 성공", true)
+	PRODUCT_DELETE_SUCCESS(HttpStatus.OK, "상품 삭제 성공", true),
+
+	/**
+	 * 201 Createed
+	 */
+	PRODUCT_CREATE_SUCCESS(HttpStatus.CREATED, "상품 생성 성공", true),
 	;
 
 	private final HttpStatus httpStatus;
