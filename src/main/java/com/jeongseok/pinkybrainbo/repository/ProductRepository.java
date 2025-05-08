7a9ad79d5e4c9ccfc6adec0e20d6a6abdaba42e4
JeongSeok <ljs0429777@gmail.com>
2025-05-08 19:26:48 +0900

    refactor: 폴더 구조 변경으로 인한 import 구문 변경

--- src/main/java/com/jeongseok/pinkybrainbo/repository/ProductRepository.java
+++ src/main/java/com/jeongseok/pinkybrainbo/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.jeongseok.pinkybrainbo.repository;
 
-import com.jeongseok.pinkybrainbo.domain.Product;
+import com.jeongseok.pinkybrainbo.domain.product.Product;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
